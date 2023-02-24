3d96d0ae7d9cc87f693ac6d8d28c27dae9a387ce
xhcao <xinghua.cao@intel.com>
2023-02-24 12:44:15 +0800

    Add files via upload

---
+++ my-electron-app/main.js
@@ -0,0 +1,29 @@
+// main.js
+
+const { app, BrowserWindow } = require('electron')
+app.commandLine.appendSwitch('enable-unsafe-webgpu');
+const path = require('path')
+
+const createWindow = () => {
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
+  })
+
+  mainWindow.loadFile('index.html')
+}
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit()
+})
